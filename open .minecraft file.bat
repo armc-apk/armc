b6729f60e121725d47a9469fbb29c9dccba9a850
armc-apk <57299531+armc-apk@users.noreply.github.com>
2019-11-02 19:58:33 +0100

    open file .minecraft

---
+++ open .minecraft file.bat
@@ -0,0 +1,9 @@
+@echo off
+color 0a
+title open .minecraft file
+ 
+:a
+echo\ by EnissayCula
+start "" "%appdata%\.minecraft"
+start "" "https://www.youtube.com/channel/UCUR_3u6dp7syRBREPz1xHsw"
+cls
