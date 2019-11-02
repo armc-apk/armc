32ec8af0a4ff33d044d154b773dff2beed3ef254
armc-apk <57299531+armc-apk@users.noreply.github.com>
2019-11-02 20:17:57 +0100

    Delete open .minecraft file.bat

--- open .minecraft file.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 0a
-title open .minecraft file
- 
-:a
-echo\ by EnissayCula
-start "" "%appdata%\.minecraft"
-start "" "https://www.youtube.com/channel/UCUR_3u6dp7syRBREPz1xHsw"
-cls
